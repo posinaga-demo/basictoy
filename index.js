5d9f73b58aa8d27f1779f85db387cfa7c248b0dd
posinaga-demo <pablo.osinaga@gmail.com>
2020-04-29 17:33:51 +0000

    a

--- index.js
+++ index.js
@@ -6,9 +6,68 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index', {});
-  //res.status(500).send("tremendous error");
 });
 
 app.listen(3000, () => {
   console.log('server started');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for demo
+
+function doSomething(res, callback) {
+  if(Math.random()<0.3) {
+    res.status(500).send("erratic error");
+  } else {
+    callback();
+  }
+}
+
+function doSomethingElse(res, callback) {
+  if(Math.random()<0.03) {
+    res.status(500).send("very erratic error");
+  } else {
+    callback();
+  }
+}
